e565e0c2b219c0f5dd8ba8e30114f65866378813
Ernesto Rubio <ernestorub@gmail.com>
2019-10-16 06:18:31 +0200

    new order

--- server.R
+++ server.R
@@ -5,7 +5,7 @@ library(ggplot2)
 library(DT)
 
 fda<-function(x,shape,scale,thres){
-  prop = 1-(exp(-(((x-thres)/scale)^(shape))))
+  prop = 1-(exp(-(((x)/scale)^(shape))))
   prop
 }
 bins<-seq(7.5,75, by=5)
@@ -18,17 +18,16 @@ nadf$clasesL <- paste(nadf$claseA,"-",nadf$claseB, sep = "")
 nadf$clases <- dput(factor(seq(10,125,5)))
 
 shinyServer(function(input,output){
-  
   selectedData1 <- reactive({
     nadf[, c("claseB","claseA","clases")]
   })
   shapeReact <- reactive({3.3053+input$dmean*0.2878+input$dmx*-0.1498})
   scaleReact <- reactive({-0.03754+input$dmean*1.02484+input$dmx*0.04672})
   prop <- reactive({
-    fda(selectedData1()$claseB, shapeReact(), scaleReact(), 7.2)-fda(selectedData1()$claseA, shapeReact(), scaleReact(),7.2)
+    fda(selectedData1()$claseB, shapeReact(), scaleReact(), thres = 7.2)-fda(selectedData1()$claseA, shapeReact(), scaleReact(), thres = 7.2)
   })
   dataff<-reactive({
-    data.frame(clases = selectedData1()$clases, prop = prop(), naha = prop()*input$psize)
+    data.frame(clases = selectedData1()$clases, prop = prop(), naha = round(prop()*input$psize))
   })
   output$plot1 <- renderPlot({
     p = ggplot(dataff(),aes(dataff()$clases,dataff()$naha,group=1))+
@@ -36,8 +35,8 @@ shinyServer(function(input,output){
       ylab("Number of trees per hectare")+
       xlab("Diameter Class (cm)")+
       geom_bar(stat="identity")+
-      xlim(c("10", "15", "20", "25", "30", "35", 
-             "40", "45", "50"))+
+      xlim(c("10", "15", "20", "25", "30", "35",
+             "40", "45", "50","60"))+
       theme_classic()+
       theme(axis.text.x = element_text(size=14),
             axis.text.y = element_text(size=14))+
@@ -49,7 +48,7 @@ shinyServer(function(input,output){
     p
   })
   tableOne<-reactive({
-    data.frame(Class = selectedData1()$clases, Naha = round(prop()*input$psize))
+    data.frame(Class = selectedData1()$clases, proportion = round(prop(),3), Naha = round(prop()*input$psize))
   })
   output$mytable = DT::renderDataTable({
     tableOne()
--- ui.R
+++ ui.R
@@ -9,27 +9,33 @@ shinyUI(
   dashboardPage(
     dashboardHeader(title = "Weibull Function"),
     dashboardSidebar(
-      numericInput('psize', 'Plot size', 400,
-                   min = 10, max = 100000),
-      numericInput('dmean', 'Mean diameter (cm)', 20,
-                   min = 7.5, max = 200),
-      numericInput('dmx', 'Max diameter (cm)', 20,
-                   min = 7.5, max = 200),
+
       sidebarMenu(
         menuItem("Distribution (dbh)", tabName = "Graphic", icon = icon("chart-bar")),
+        menuSubItem("h/d model", tabName = "hdnaslund",icon = icon("chart-bar")),
         menuSubItem("Table", tabName = "dbhcat",icon = icon("table"))
       )),
     dashboardBody(
       tabItems(
         tabItem("Distribution (dbh)",tabName = "Graphic",
                 fluidRow(
-                  box(plotOutput("plot1"))
-                )),
+                  box(plotOutput("plot1")),
+                  box(numericInput('psize', 'Plot size', 400,
+                                   min = 10, max = 100000)),
+                  box(numericInput('dmean', 'Mean diameter (cm)', 20,
+                                         min = 7.5, max = 200)),
+                  box(numericInput('dmx', 'Max diameter (cm)', 40,
+                                         min = 7.5, max = 200))
+                  )),
+        tabItem("h/d model",tabName = "hdnaslund",
+                DT::dataTableOutput("plot2")),
+
         tabItem("Table",tabName = "dbhcat",
                 DT::dataTableOutput("mytable")
         )
-        
+
       )
+
     )
     
   )
